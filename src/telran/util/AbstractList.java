4ab6db0a03d246e9df16f5d99a409bdaff55b605
ikrasnova <i.bas@me.com>
2021-11-10 22:59:44 +0200

    hw11

---
+++ src/telran/util/AbstractList.java
@@ -0,0 +1,18 @@
+package telran.util;
+
+public abstract class AbstractList<T> implements List<T> {
+
+protected int size;
+
+
+	@Override
+	public int size() {
+		
+		return size;
+	}
+
+	protected boolean isValidIndex(int index)
+	{
+		return index >= 0 && index < size;
+	}
+}
